7a0bc35ca4c6b9fb212656bf1b64c020b9ea4a11
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2023-10-25 00:54:50 -0500

    Added more functions and psuedo coding

--- index.js
+++ index.js
@@ -78,10 +78,10 @@ function viewAllRoles() {
         console.table(result);
     })
 }
-
+// TODO remove column with array numbers and figure out manager_id
 function viewAllEmployees() {
     const queryString = `
-    
+    SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS "department", role.salary, employee.manager_id FROM employee INNER JOIN role ON role.id = employee.role_id INNER JOIN department on role.department_id = department.id;
     `
     db.query(queryString, (err, result) => {
         if (err) {
@@ -92,35 +92,102 @@ function viewAllEmployees() {
     })
 }
 
-// addDepartment
-// addRole
-// addEmployee
-// updateRole
+// TODO finish
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: 'text',
+            message: 'Enter the name of the department you want to add:',
+            name: 'dptName',
+        }
+    ]).then(answers => {
+        const newDepartment = answers.dptname
+        console.log(newDepartment)
+        const queryString = `
+        INSERT INTO department (name) VALUES ('$answers.dptName')
+        `
+    })
+}
+
+// TODO finish
+function addRole() {
+    inquirer.prompt([
+        {
+            type: 'text',
+            message: 'Enter the name of the role you want to add:',
+            name: 'roleName',
+        }
+    ]).then(answers => {
+        const newRole = answers.roleName
+        console.log(newDepartment)
+        const queryString = `
+        INSERT INTO role (name) VALUES ('$answers.roleName')
+        `
+    })
+}
+
+// TODO finish
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: 'text',
+            message: "What is the employee's first name?",
+            name: 'newFirst_Name',
+        },
+        {
+            type: 'text',
+            message: "What is the employee's last name?",
+            name: 'newLast_Name',
+        },
+        {
+            type: 'list',
+            message: "What is the employee's role?",
+            name: 'newEmRole',
+            choices: []
+        },
+        {
+            type: 'list',
+            message: "Who is the employee's manager?",
+            name: 'newManager',
+            choices: []
+        },
+    ]).then(answers => {
+        const queryString = `
+       
+        `
+    })
+}
+
+function updateRole() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: "Which employee's role do you want to update?",
+            name: 'updateRoleEmployee',
+            choices: []
+        },
+        {
+            type: 'list',
+            message: "Which role do you want to assign the selected employee?",
+            name: 'updateRoleTitle',
+            choices: []
+        },
+}
 
 startUp()
 
 
 // WHEN I choose to view all employees
 // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-        // SELECT * FROM employee
 
 // WHEN I choose to add a department
 // THEN I am prompted to enter the name of the department and that department is added to the database
-        // INSERT INTO department(column_list)
-        // VALUES(value_list)
 
 // WHEN I choose to add a role
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-        // INSERT INTO role(column_list)
-        // VALUES(value_list)
 
 // WHEN I choose to add an employee
 // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-        // INSERT INTO employee(column_list)
-        // VALUES(value_list)
 
 // WHEN I choose to update an employee role
 // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
-        // UPDATE role
-        // SET column_1 = value_1,
-        // WHERE condition
