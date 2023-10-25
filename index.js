58a46f26bcbc85ee9e692e87f878ac755694bfe5
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2023-10-25 17:34:14 -0500

    added title art. Halfway done refactoring to async await functions.

--- index.js
+++ index.js
@@ -1,112 +1,146 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
+const titleArt = (`
+**************************************************************************
+  _______  __   __  _______  ___      _______  __   __  _______  _______ 
+ |       ||  |_|  ||       ||   |    |       ||  | |  ||       ||       |
+ |    ___||       ||    _  ||   |    |   _   ||  |_|  ||    ___||    ___|
+ |   |___ |       ||   |_| ||   |    |  | |  ||       ||   |___ |   |___ 
+ |    ___||       ||    ___||   |___ |  |_|  ||_     _||    ___||    ___|
+ |   |___ | ||_|| ||   |    |       ||       |  |   |  |   |___ |   |___ 
+ |_______||_|   |_||___|    |_______||_______|  |___|  |_______||_______|
+     _______  ______    _______  _______  ___   _  _______  ______       
+    |       ||    _ |  |   _   ||       ||   | | ||       ||    _ |      
+    |_     _||   | ||  |  |_|  ||       ||   |_| ||    ___||   | ||      
+      |   |  |   |_||_ |       ||       ||      _||   |___ |   |_||_     
+      |   |  |    __  ||       ||      _||     |_ |    ___||    __  |    
+      |   |  |   |  | ||   _   ||     |_ |    _  ||   |___ |   |  | |    
+      |___|  |___|  |_||__| |__||_______||___| |_||_______||___|  |_|    
+
+**************************************************************************       
+`);
+
+console.log(titleArt);
 
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user:'root',
-        password:'rootytooty',
+        user: 'root',
+        password: 'rootytooty',
         database: 'employees_db'
     },
     console.log(`Connected to the employees_db database.`)
 );
 
-db.query('SELECT * FROM employees', function (err, results) {
-    console.log(results);
-});
+// db.query('SELECT * FROM employees', function (err, results) {
+//     console.log(results);
+// });
 
-function startUp() {
-    inquirer.prompt([
+async function startUp() {
+    const answers = await inquirer.prompt([
         {
             type: 'list',
             message: 'What would you like to do?',
             name: 'action',
             choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
         }
-    ]).then(answers => {
-        switch (answers.action) {
-            case "View all departments":
-                viewAllDepartments();
-                break;
-            case "View all roles":
-                viewAllRoles();
-                break;
-            case "View all employees":
-                viewAllEmployees();
-            case "Add a department":
-                addDepartment();
-                break;
-            case "Add a role":
-                addRole();
-                break;
-            case "Add an employee":
-                addEmployee();
-                break;
-            case "Update an employee role":
-                updateRole();
-                break;
-            default:
-                return;
-        } 
-         startUp();   
-    })
-}
+    ])
 
-function viewAllDepartments() {
-    const queryString = `
-    SELECT * FROM department
-    `
-    db.query(queryString, (err, result) => {
-        if (err) {
-            console.log("Oops! Trouble showing all departments.", err);
+    switch (answers.action) {
+        case "View all departments":
+            await viewAllDepartments();
+            break;
+        case "View all roles":
+            await viewAllRoles();
+            break;
+        case "View all employees":
+            await viewAllEmployees();
+            break;
+        case "Add a department":
+            addDepartment();
+            break;
+        case "Add a role":
+            addRole();
+            break;
+        case "Add an employee":
+            await addEmployee();
+            break;
+        case "Update an employee role":
+            updateRole();
+            break;
+        default:
             return;
-        }
-        console.table(result);
-    })
+    }
+
+    startUp();
 }
 
-// TODO figure out how to remove column with array numbers?
-function viewAllRoles() {
-    const queryString = `
+async function viewAllDepartments() {
+    try {
+        const queryString = `SELECT * FROM department`
+        const [departments] = await db.promise().query(queryString);
+        console.table(departments);
+
+    } catch (err) {
+        console.log("Oops! Trouble showing all departments.", err);
+    }
+
+}
+
+async function viewAllRoles() {
+    try {
+        const queryString = `
     SELECT role.id, title, name AS department, salary FROM role INNER JOIN department ON department_id = department.id;
     `
-    db.query(queryString, (err, result) => {
-        if (err) {
-            console.log("Oops! Trouble showing all roles.", err);
-            return;
-        }
-        console.table(result);
-    })
+    const [roles] = await db.promise().query(queryString);
+    console.table(roles);
+    } catch (err) {
+        console.log("Oops! Trouble showing all roles.", err);
+    }   
 }
-// TODO remove column with array numbers and figure out manager_id
-function viewAllEmployees() {
-    const queryString = `
-    SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS "department", role.salary, employee.manager_id FROM employee INNER JOIN role ON role.id = employee.role_id INNER JOIN department on role.department_id = department.id;
+
+async function viewAllEmployees() {
+    try {
+        const queryString = `
+    SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS "department", role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager
+    FROM employee 
+    INNER JOIN role 
+        ON role.id = employee.role_id 
+    INNER JOIN department
+        on role.department_id = department.id
+    JOIN employee AS manager
+        on employee.manager_id = manager.id;
     `
-    db.query(queryString, (err, result) => {
-        if (err) {
-            console.log("Oops! Trouble showing all employees.", err);
-            return;
-        }
-        console.table(result);
-    })
+        const [employees] = await db.promise().query(queryString);
+        console.table(employees);
+    } catch (err) {
+        console.log("Oops! Trouble showing all employees.", err);
+    }
 }
 
 // TODO finish
-function addDepartment() {
-    inquirer.prompt([
+async function addDepartment() {
+    const newDepartment = await inquirer.prompt([
         {
             type: 'text',
             message: 'Enter the name of the department you want to add:',
-            name: 'dptName',
+            name: 'name',
         }
-    ]).then(answers => {
-        const newDepartment = answers.dptname
-        console.log(newDepartment)
-        const queryString = `
-        INSERT INTO department (name) VALUES ('$answers.dptName')
-        `
-    })
+    ])
+    try {
+        const [department] = await db.promise().query(queryString);
+        console.table(department);
+    } catch (err) {
+        console.log("Something went wrong", err)
+    }
+    // .then(answers => {
+    //     const newDepartment = answers.dptname
+    //     console.log(newDepartment)
+    //     const queryString = `
+    //     INSERT INTO department (name) VALUES ('${answers.dptName}')
+    //     `
+    // })
 }
 
 // TODO finish
@@ -127,52 +161,84 @@ function addRole() {
 }
 
 // TODO finish
-function addEmployee() {
-    inquirer.prompt([
-        {
-            type: 'text',
-            message: "What is the employee's first name?",
-            name: 'newFirst_Name',
-        },
-        {
-            type: 'text',
-            message: "What is the employee's last name?",
-            name: 'newLast_Name',
-        },
-        {
-            type: 'list',
-            message: "What is the employee's role?",
-            name: 'newEmRole',
-            choices: []
-        },
-        {
-            type: 'list',
-            message: "Who is the employee's manager?",
-            name: 'newManager',
-            choices: []
-        },
-    ]).then(answers => {
-        const queryString = `
-       
-        `
-    })
+async function addEmployee() {
+    try {
+        const roleChoices = await getAllRoles();
+        const managerChoices = await getAllManagers();
+
+        const newEmployee = await inquirer.prompt([
+            {
+                type: 'text',
+                message: "What is the employee's first name?",
+                name: 'first_name',
+            },
+            {
+                type: 'text',
+                message: "What is the employee's last name?",
+                name: 'last_name',
+            },
+            {
+                type: 'list',
+                message: "What is the employee's role?",
+                name: 'role_id',
+                choices: roleChoices
+            },
+            {
+                type: 'list',
+                message: "Who is the employee's manager?",
+                name: 'manager_id',
+                choices: managerChoices
+            },
+        ])
+
+        await db.promise().query("INSERT INTO employee SET ?", newEmployee);
+        console.log(`${newEmployee.first_name} ${newEmployee.last_name} has been added!`);
+    } catch (err) {
+        console.log("Oops! Trouble showing all employees.", err);
+    }
+
 }
 
 function updateRole() {
-    inquirer.prompt([
-        {
-            type: 'list',
-            message: "Which employee's role do you want to update?",
-            name: 'updateRoleEmployee',
-            choices: []
-        },
-        {
-            type: 'list',
-            message: "Which role do you want to assign the selected employee?",
-            name: 'updateRoleTitle',
-            choices: []
-        },
+    // inquirer.prompt([
+    //     {
+    //         type: 'list',
+    //         message: "Which employee's role do you want to update?",
+    //         name: 'updateRoleEmployee',
+    //         choices: []
+    //     },
+    //     {
+    //         type: 'list',
+    //         message: "Which role do you want to assign the selected employee?",
+    //         name: 'updateRoleTitle',
+    //         choices: []
+    //     },
 }
+
+async function getAllRoles() {
+    try {
+        const [roleChoices] = await db.promise().query(`Select id AS value, title AS name FROM role`);
+        return roleChoices;
+    } catch (err) {
+        console.log("Somethings went wrong", err)
+    }
+
+}
+
+async function getAllManagers() {
+    try {
+        const queryString = `
+        Select id AS value, CONCAT(first_name, " ", last_name) AS name
+             FROM employee
+        `
+        const [managerChoices] = await db.promise().query(queryString);
+        return managerChoices;
+    } catch (err) {
+        console.log("Somethings went wrong", err)
+    }
+
+}
+
 
 startUp()
 
